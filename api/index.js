24d74491f8ae074204be81eee708bf281710d5ee
coderRahul212000 <rahulrajput211998@gmail.com>
2025-07-23 21:21:49 +0530

    final changes

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
-app.set("view", path.join(__dirname, "views"));  
+app.set("views", path.join(__dirname, "views"));  
 
 
 app.get('/', (req,res)=>{
