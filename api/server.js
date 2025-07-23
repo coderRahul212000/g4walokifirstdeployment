2b6effde99644e9e694bcc3cfdeb0f9215d5a691
coderRahul212000 <rahulrajput211998@gmail.com>
2025-07-23 20:55:14 +0530

    mera first deployment hai ye

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const chalk = require('chalk');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set("view", path.join(__dirname, "views"));  
+
+
+app.get('/home', (req,res)=>{
+    const name = req.params.name
+    res.render("index")
+})
+
+module.exports = app;
